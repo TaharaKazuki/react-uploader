02fa7965a862bccda9d21499027f275af0543b0d
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-19 20:32:49 +0900

    update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-primary">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Uploader/Uploader.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Uploader = () => {
+  return <div></div>
+}
